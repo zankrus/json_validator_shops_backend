755607984e2895ff36caf935bcc8945c2b62ce36
Ro <sealthepirate@gmail.com>
2020-05-26 09:33:46 +0300

    Added database connector and main file

---
+++ Data_base.py
@@ -0,0 +1,42 @@
+import psycopg2
+
+
+def data_base_creating():
+    con = psycopg2.connect(
+      database="postgres",
+      user="postgres",
+      password="221052",
+      host="127.0.0.1",
+      port="5432"
+    )
+    cur = con.cursor()
+
+    cur.execute('''DROP TABLE IF EXISTS GOODS''')
+
+    cur.execute('''CREATE TABLE GOODS  
+         (ID SERIAL  PRIMARY KEY NOT NULL ,
+         NAME VARCHAR NOT NULL,
+         PACKAGE_HEIGHT FLOAT NOT NULL,
+         PACKAGE_WIDTH FLOAT NOT NULL);''')
+
+
+    cur.execute('''DROP TABLE IF EXISTS SHOPS_GOODS''')
+
+    cur.execute('''CREATE TABLE SHOPS_GOODS  
+         (ID SERIAL  PRIMARY KEY NOT NULL ,
+         ID_GOOD INT NOT NULL,
+         LOCATION VARCHAR NOT NULL,
+         AMOUTH INT NOT NULL,
+         FOREIGN KEY (ID_GOOD)  REFERENCES GOODS (ID));''')
+
+    con.commit()
+    con.close()
+
+def insert_bd():
+    pass
+
+def update_bd():
+    pass
+
+
+
